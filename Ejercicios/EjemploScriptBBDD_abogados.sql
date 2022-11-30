582f0ee383f023ec913020a7d69b318f86314e18
bernat <bernat13@gmail.com>
2022-11-30 08:50:28 +0100

    ejemplo script bbdd

---
+++ Ejercicios/EjemploScriptBBDD_abogados.sql
@@ -0,0 +1,17 @@
+use master
+
+GO
+drop database if exists nombreApellido_bufete
+GO
+create database nombreApellido_bufete
+GO
+use nombreApellido_bufete
+
+GO
+
+CREATE TABLE Abogado(
+id int IDENTITY(1,1) PRIMARY KEY,
+nombre varchar(100) not null
+
+)
+

ae82a424afbbb951933bb46445cfa89f41bbc4e8
bernat <bernat13@gmail.com>
2022-11-30 12:44:02 +0100

    modificacion script ejemplo

--- Ejercicios/EjemploScriptBBDD_abogados.sql
+++ Ejercicios/EjemploScriptBBDD_abogados.sql
@@ -1,18 +1,26 @@
 use master
-
 GO
 drop database if exists nombreApellido_bufete
 GO
 create database nombreApellido_bufete
 GO
 use nombreApellido_bufete
-
 GO
 
+--Ejercicio 1
 CREATE TABLE Abogado(
 id int IDENTITY(1,1) PRIMARY KEY,
 nombre varchar(100) not null
-
 )
 
+--Ejercicio 2
+--Bernat, he interpretado esto como una 1N
+
+create table asunto(
+    id int primary KEY,
+    id_abogado int not null,
+    constraint fkabo FOREIGN KEY (id_abogado) REFERENCES abogado(id)
+)
 PRINT 'YA HE ACABADO'
+
+
